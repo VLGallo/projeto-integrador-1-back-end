359f97eba9df8040628765f224e54531d990c9b2
Vanessa Leticia Gallo <96696812+VLGallo@users.noreply.github.com>
2024-03-23 14:46:50 -0300

    atualiza o crud de gerenciador de motoboys

--- back-end/gerenciador_de_motoboys/views.py
+++ back-end/gerenciador_de_motoboys/views.py
@@ -1,3 +1,5 @@
+from rest_framework.exceptions import NotFound
+
 from gerenciador_de_motoboys.models import Motoboy
 from gerenciador_de_motoboys.serializers import MotoboySerializer
 from rest_framework import status
@@ -15,10 +17,49 @@ class MotoboyView(APIView):
 
         return Response(data=response_data, status=status.HTTP_201_CREATED)
 
-
-class MotoboysListView(APIView):
+class MotoboyListView(APIView):
     def get(self, request):
         companies = Motoboy.objects.all()
         serializer = MotoboySerializer(companies, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class MotoboyDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Motoboy.objects.get(pk=pk)
+        except Motoboy.DoesNotExist:
+            raise NotFound("Motoboy não encontrado")
+
+    def get(self, request, pk):
+        motoboy = self.get_object(pk)
+        serializer = MotoboySerializer(motoboy)
+        return Response(serializer.data)
+
+class MotoboyUpdateView(APIView):
+    def get_object(self, pk):
+        try:
+            return Motoboy.objects.get(pk=pk)
+        except Motoboy.DoesNotExist:
+            raise NotFound("Motoboy não encontrado")
+
+    def put(self, request, pk):
+        motoboy = self.get_object(pk)
+        serializer = MotoboySerializer(motoboy, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class MotoboyDeleteView(APIView):
+    def get_object(self, pk):
+        try:
+            return Motoboy.objects.get(pk=pk)
+        except Motoboy.DoesNotExist:
+            raise NotFound("Motoboy não encontrado")
+
+    def delete(self, request, pk):
+        motoboy = self.get_object(pk)
+        motoboy.delete()
+        return Response(status=status.HTTP_202_ACCEPTED, data="Motoboy deletado com sucesso")
